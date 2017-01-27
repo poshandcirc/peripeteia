55f93b2fd15ad8e679040b62f1741218fef51f77
poshandcirc <cassystone@gmail.com>
2017-01-26 21:27:24 -0500

    messages

--- PeripeteiaFortuneTeller/PeripeteiaFortuneTeller/ViewController.swift
+++ PeripeteiaFortuneTeller/PeripeteiaFortuneTeller/ViewController.swift
@@ -20,16 +20,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    let messages: [String] = ["You're in luck!", "Try Again", "Maybe not...", "All signs point to yes.", "I'm not sure. You've stumped me."]
+    
+    func randomMessage(messArray: [String]) -> String {
+        let randNum = Int(arc4random_uniform(UInt32(messArray.count + 1)))
+        return messArray[randNum]
+    }
+    
     @IBAction func giveFortune(_ sender: UIButton) {
         
-        let alert = UIAlertController(title: "YOUR FUTURE", message: "kys", preferredStyle: UIAlertControllerStyle.alert)
+        let alert = UIAlertController(title: "YOUR FUTURE", message: randomMessage(messArray: messages), preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "DONE", style: UIAlertActionStyle.default, handler: nil))
         
         let window = UIApplication.shared.windows[0]
         window.rootViewController?.present(alert, animated: true, completion: nil)
     }
-
-
-
 }
 
